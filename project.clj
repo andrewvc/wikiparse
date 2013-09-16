9fe689b68c5001ad04a55aa34c013fc5f510c10d
Andrew Cholakian <andrew@andrewvc.com>
2013-09-15 21:29:53 -0700

    Improve project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :main wikiparse.core
-  :jvm-opts ["-Xmx1500m" "-server"]
+  :jvm-opts ["-256m" "-server"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
